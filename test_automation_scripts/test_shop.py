3a932237d911bf9c886656df2fa99c5fba916988
Sithiqu <sithiqu.dev@gmail.com>
2025-10-19 17:34:58 +0530

    Update test_shop.py

--- test_automation_scripts/test_shop.py
+++ test_automation_scripts/test_shop.py
@@ -1,17 +1,2 @@
-import pytest
-
-@pytest.mark.fruits
-def test_apple():
-   print("This is Apple function")
-
-@pytest.mark.fruits
-def test_banana():
-   print("This is Banana function")
-
-@pytest.mark.veg
-def test_carrot():
-   print("This is Carrot function")
-  
-@pytest.mark.veg
-def test_beetroote():
-    print("This is Beetroote function")
+with open('myfile1.txt","w") as f1:
+          f1.write("hi")
